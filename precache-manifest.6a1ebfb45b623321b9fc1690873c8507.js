47bd7bd86876b6d7fca3b79f15c4fc77fcb6213d
Joel Armendariz <joelarmendariz@Joels-MBP.attlocal.net>
2020-05-09 19:01:25 -0500

    Updates

---
+++ precache-manifest.6a1ebfb45b623321b9fc1690873c8507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f330cc476b67d8aa5f387857fcb9d6",
+    "url": "/farkle-counter/index.html"
+  },
+  {
+    "revision": "da8d3a6f0dd7930e0714",
+    "url": "/farkle-counter/static/css/main.2cf56254.chunk.css"
+  },
+  {
+    "revision": "79a9f769d0db9ebf3544",
+    "url": "/farkle-counter/static/js/2.f3fb4386.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/farkle-counter/static/js/2.f3fb4386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da8d3a6f0dd7930e0714",
+    "url": "/farkle-counter/static/js/main.49bbd0cf.chunk.js"
+  },
+  {
+    "revision": "c2b3535d40731f3dc7cf",
+    "url": "/farkle-counter/static/js/runtime-main.45533218.js"
+  }
+]);
